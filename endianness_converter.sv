f5a42249e5433cfa6a89409ec58c65471a9566f9
zimmshane <zimmshane@gmail.com>
2026-04-09 10:40:24 -0700

    input reg fix

--- endianness_converter.sv
+++ endianness_converter.sv
@@ -1,6 +1,6 @@
 module endianness_converter(
     input clk,
-    input reg [31:0] word,
+    input [31:0] word,
     input endianness_in,
     input endianness_out,
     output reg [31:0] converted_word
@@ -8,9 +8,9 @@ module endianness_converter(
 
 always @(posedge clk ) begin
     if (endianness_in == endianness_out) begin
-        converted_word = word;
+        converted_word <= word;
     end else begin
-        converted_word = {word[7:0], word[15:8],word[23:16],word[31:24]};
+        converted_word <= {word[7:0], word[15:8],word[23:16],word[31:24]};
     end
 end
     
